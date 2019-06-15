368fcb502b073b299d822da5e652fe7570a4db89
chaogabriel <fusionpol@hotmail.com>
2019-06-15 23:02:19 +0800

    Changed server\scrape to return dict of modules

--- server/scrape/index.js
+++ server/scrape/index.js
@@ -1,19 +1,13 @@
 const puppeteer = require('puppeteer');
+const qs = require('query-string');
 
 async function scrape(shareurl) {
     console.log(`Received url ${shareurl}`);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(shareurl);
-    
-    const timetableurl = await page.evaluate(() => {
-        // The dropdown menu will either have id=downshift-0 or id=downshift1. Try both
-        const element0 = document.getElementById("downshift-0-item-0");
-        const element1 = document.getElementById("downshift-1-item-0");
-        return element0 ? element0.href : element1.href;
-    });
     await browser.close();
-    return timetableurl;
+    return qs.parseUrl(page.url()).query;
 }
 
 module.exports = scrape;
