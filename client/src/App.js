6bc16c1e8633c4afb62ecb67eb1ffadc9095fd65
Tobias Yap <tobias.yap@gmail.com>
2019-06-19 02:36:56 +0800

    Configuration for app UI.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  Container,
+  Navbar,
+  NavbarBrand,
+  Row,
+  Col,
+} from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="Centered">
+      <Navbar dark color="dark">
+        <NavbarBrand href="/">bzNUS</NavbarBrand>
+      </Navbar>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 }
 
