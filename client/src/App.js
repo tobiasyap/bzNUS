52d00c81d7099d020afe6c67f422dca4e70bbfdf
Tobias Yap <tobias.yap@gmail.com>
2019-07-02 02:30:29 +0900

    updated for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
 
 import {
   BrowserRouter as Router,
@@ -19,23 +21,86 @@ import GroupPage from './pages/group';
 import NavBar from './components/Navbar';
 import PrivateRoute from './components/privateroute';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      error: null,
+      authenticated: false
+    };
+  }
 
-  const NavigationBar = withRouter(NavBar);
+  static propTypes = {
+    user: PropTypes.shape({
+      username: PropTypes.string,
+      fullname: PropTypes.string,
+      email: PropTypes.string,
+      timtableurl: PropTypes.string,
+      user_id: PropTypes.number,
+      nusnet_id: PropTypes.string,
+      group_ids: PropTypes.array
+    })
+  };
 
-  return (
-    <Router>
-        <NavigationBar />
-        <Switch>
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/404" component={NotFoundPage} />
-          <Route exact path="/nusmods" component={NusModsPage} />
-          <Route exact path="/group" component={GroupPage} />
-          <Redirect to="/404" />
-        </Switch>
-    </Router>
-  );
+  componentDidMount() {
+    // Fetch does not send cookies. So you should add credentials: 'include'
+    fetch("http://localhost:5000/auth/login/success", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+      }
+    })
+      .then(response => {
+        if (response.status === 200) return response.json();
+        throw new Error("failed to authenticate user");
+      })
+      .then(responseJson => {
+        this.setState({
+          authenticated: true,
+          user: responseJson.user
+        });
+      })
+      .catch(error => {
+        this.setState({
+          authenticated: false,
+          error: "Failed to authenticate user"
+        });
+      });
+  }
+
+  render() {
+    const NavigationBar = withRouter(NavBar);
+    const { authenticated } = this.state;
+
+    return (
+      <Router>
+          <NavigationBar />
+          <Switch>
+            <Route exact path="/login" component={LoginPage} />
+            {!authenticated ? (
+              <Redirect to="/login" />
+            ) : (
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/404" component={NotFoundPage} />
+                <Route exact path="/nusmods" component={NusModsPage} />
+                <Route exact path="/group" component={GroupPage} />
+              </Switch>
+            )}
+            
+            <Redirect to="/404" />
+          </Switch>
+      </Router>
+    );
+  }
+  
+  _handleNotAuthenticated = () => {
+    this.setState({ authenticated: false });
+  };
 }
 
 export default App;
