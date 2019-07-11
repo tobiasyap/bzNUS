90644c64b54eea7bc49a89ea6702f6bdd8ad955c
chaogabriel <fusionpol@hotmail.com>
2019-07-12 01:35:41 +0800

    Fix timetable not being stored in database

--- server/models/Timetable.js
+++ server/models/Timetable.js
@@ -2,20 +2,25 @@
  * Module with database functions for interacting with Timetable objects.
  */
 
-const db = require('../database');
+const db = require("../database");
 
 function findByTimetableUrl(timetableurl) {
-    return db.one('SELECT * FROM timetableurl_timetables WHERE timetableurl = $1', timetableurl);
+  return db
+    .one(
+      "SELECT timetable FROM timetableurl_timetables WHERE timetableurl = $1",
+      timetableurl
+    )
+    .then(row => row.timetable);
 }
 
-function insert(t) {
-    return db.none('INSERT INTO timetableurl_timetables (timetableurl, timetable) VALUES (${url}, ${tt})', {
-        url: t.timetableurl,
-        tt: t.timetable
-    });
+function insert(timetableurl, timetable) {
+  return db.none(
+    "INSERT INTO timetableurl_timetables (timetableurl, timetable) VALUES ($1, $2)",
+    [timetableurl, timetable]
+  );
 }
 
 module.exports = {
-    findByTimetableUrl: findByTimetableUrl,
-    insert: insert
+  findByTimetableUrl: findByTimetableUrl,
+  insert: insert
 };
--- server/routes/api/nusmods.js
+++ server/routes/api/nusmods.js
@@ -1,80 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Timetable = require('../../models/Timetable');
-const nusmods = require('../../nusmods');
-const utils = require('../../../utils/timetable');
-const Global = require('../../config/Global');
+const Timetable = require("../../models/Timetable");
+const nusmods = require("../../nusmods");
+const utils = require("../../../utils/timetable");
+const Global = require("../../config/Global");
 
 const { YEAR, SEMESTER } = Global;
 
-router.get('/:url', async (req, res, next) => {
-    const url = req.params.url;
-    console.log(`Received url ${url}`);
+router.get("/:url", async (req, res, next) => {
+  const url = req.params.url;
+  console.log(`Received url ${url}`);
 
-    // To reduce API calls to NUSMods, app will cache timetables in the database
-    // First check if timetable already exists in database
-    let timetable;
+  // To reduce API calls to NUSMods, app will cache timetables in the database
+  // First check if timetable already exists in database
+  let timetable;
+  try {
+    timetable = await Timetable.findByTimetableUrl(url);
+  } catch {
+    console.log(`${url} does not exist in database.`);
+    timetable = null;
+  }
+
+  if (timetable) {
+    // Timetable exists in app database.
+    console.log("URL exists in database.");
+  } else {
+    // Timetable does not exist in app database yet.
+    // Follow the short shareURL and extract the serialized timetable
+    // from the redirect URL's query
+    let serializedTimetable;
     try {
-        timetable = await Timetable.findByTimetableUrl(url);
+      serializedTimetable = await nusmods.getSerializedTimetable(url);
+    } catch (err) {
+      res.status(404).send(`The url '${url}' is not valid.`);
+      console.error(err);
+      return;
     }
-    catch {
-        console.log(`${url} does not exist in database.`);
-        timetable = null;
-    }
-    
-    if(timetable) {
-        // Timetable exists in app database.
-        console.log('URL exists in database.');
-    }
-    else { 
-        // Timetable does not exist in app database yet.
-        // Follow the short shareURL and extract the serialized timetable
-        // from the redirect URL's query
-        let serializedTimetable;
-        try {
-            serializedTimetable = await nusmods.getSerializedTimetable(url);
-        }
-        catch(err) {
-            res.status(404).send(`The url '${url}' is not valid.`);
-            console.error(err);
-            return;
-        }
-        console.log(`Serialized to ${serializedTimetable}`);
-        const codedTimetable = utils.deserializeTimetable(serializedTimetable);
-        if(!codedTimetable) {
-            res.status(404).send(`Error deserializing '${url}'`);
-            return;
-        }
-        
-        // Serialized timetable has been obtained, but it only contains module
-        // codes, not module info. Call NUSMods API to retrieve each module's info
-        timetable = {};
-        let promises = [];
-        for(const [moduleCode, codedTimes] of Object.entries(codedTimetable)) {
-            modPromise = nusmods.getModule(YEAR, moduleCode)
-                .then((moduleData) => { 
-                    timetable[moduleCode] = utils.decodeTimes(moduleData, SEMESTER, codedTimes);
-                });
-            promises.push(modPromise);
-        }
-        try {
-            await Promise.all(promises);
-        }
-        catch(err) {
-            res.status(404).send("Error fetching module data.");
-            console.error(err);
-            return;
-        }
-
-        // Insert timetable into app database so it can be retrieved 
-        // by future queries to this URL
-        Timetable.insert({url, timetable});
+    console.log(`Serialized to ${serializedTimetable}`);
+    const codedTimetable = utils.deserializeTimetable(serializedTimetable);
+    if (!codedTimetable) {
+      res.status(404).send(`Error deserializing '${url}'`);
+      return;
     }
 
-    // Finally, send the full timetable to the client
-    res.send(timetable);
-    console.log(timetable);
+    // Serialized timetable has been obtained, but it only contains module
+    // codes, not module info. Call NUSMods API to retrieve each module's info
+    timetable = {};
+    let promises = [];
+    for (const [moduleCode, codedTimes] of Object.entries(codedTimetable)) {
+      modPromise = nusmods.getModule(YEAR, moduleCode).then(moduleData => {
+        timetable[moduleCode] = utils.decodeTimes(
+          moduleData,
+          SEMESTER,
+          codedTimes
+        );
+      });
+      promises.push(modPromise);
+    }
+    try {
+      await Promise.all(promises);
+    } catch (err) {
+      res.status(404).send("Error fetching module data.");
+      console.error(err);
+      return;
+    }
+
+    // Insert timetable into app database so it can be retrieved
+    // by future queries to this URL
+    Timetable.insert(url, timetable).catch(err => {
+      console.error("Error inserting timetable.");
+    });
+  }
+
+  // Finally, send the full timetable to the client
+  res.send(timetable);
+  console.log(timetable);
 });
 
 module.exports = router;
