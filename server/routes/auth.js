7f74124fba345d6bc39452d90c91770b35b01170
chaogabriel <fusionpol@hotmail.com>
2019-07-29 23:43:27 +0800

    Add log statement for debugging

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,47 +1,50 @@
-const router = require('express').Router();
-const passport = require('@passport-next/passport');
+const router = require("express").Router();
+const passport = require("@passport-next/passport");
 
-const Global = require('../config/Global');
+const Global = require("../config/Global");
 
 const CLIENT_HOME_PAGE_URL = Global.CLIENT_HOME_PAGE_URL;
 
 // Set up authentication route
-router.get('/auth/nus', passport.authenticate('nus-openid'));
+router.get("/auth/nus", () => {
+  console.log("authenticating");
+  passport.authenticate("nus-openid");
+});
 
 // Redirect to client homepage after sucessful authentication
-router.get('/auth/nus/return',
-    passport.authenticate('nus-openid', {
-        successRedirect: CLIENT_HOME_PAGE_URL,
-        failureRedirect: '/auth/login/failed'
-    })
+router.get(
+  "/auth/nus/return",
+  passport.authenticate("nus-openid", {
+    successRedirect: CLIENT_HOME_PAGE_URL,
+    failureRedirect: "/auth/login/failed"
+  })
 );
 
 // Allow retrival of user info when login is successful
-router.get('/auth/login/success', (req, res) => {
-    // req.user only exists if passport has authenticated
-    if(req.user) {
-        res.json({
-            success: true,
-            message: 'user is authenticated',
-            user: req.user,
-        });
-    }
-    else {
-        res.redirect(req.baseUrl + '/auth/login/failed');
-    }
+router.get("/auth/login/success", (req, res) => {
+  // req.user only exists if passport has authenticated
+  if (req.user) {
+    res.json({
+      success: true,
+      message: "user is authenticated",
+      user: req.user
+    });
+  } else {
+    res.redirect(req.baseUrl + "/auth/login/failed");
+  }
 });
 
-router.get('/auth/login/failed', (req, res) => {
-    res.status(401).json({
-        success: false,
-        message: 'user failed to authenticate'
-    });
+router.get("/auth/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "user failed to authenticate"
+  });
 });
 
 // After logging out, redirect back to client
-router.get('/auth/logout', (req, res) => {
-    req.logout();
-    res.redirect(CLIENT_HOME_PAGE_URL);
+router.get("/auth/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_HOME_PAGE_URL);
 });
 
 module.exports = router;
