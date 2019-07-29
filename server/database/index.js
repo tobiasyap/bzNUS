6bba9eea10430c96e44dbe4febe185958e269a4c
chaogabriel <fusionpol@hotmail.com>
2019-07-29 21:38:38 +0800

    Change database initialisation to check NODE_ENV

--- server/database/index.js
+++ server/database/index.js
@@ -2,20 +2,22 @@
  * Module for connecting to and returning the PostgreSQL database.
  */
 
-const pgp = require('pg-promise')({});
+const pgp = require("pg-promise")({});
 
 // Database connection details
-const cn = {
-    host: 'localhost',
-    port: 5432,
-    database: 'bznus-db',
-    user: 'postgres',
-    password: 'devpassword'
+const localConnection = {
+  host: "localhost",
+  port: 5432,
+  database: "bznus-db",
+  user: "postgres",
+  password: "devpassword"
 };
-const CONNECTION_STRING = process.env.DATABASE_URL 
-    || 'postgresql://postgres:postgres@localhost:5432/bznus-db';
-const SSL = process.env.NODE_ENV === 'production';
+const connection =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : localConnection;
+const SSL = process.env.NODE_ENV === "production";
 
-const db = pgp(cn);
+const db = pgp(connection);
 
 module.exports = db;
